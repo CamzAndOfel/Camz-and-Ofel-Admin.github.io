38523761ff23879ed32f7e2626eac9c2d85e3662
miggy13 <100565825+miggy13@users.noreply.github.com>
2022-07-02 19:40:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function check(form) {
     if (form.userid.value == "Admin" && form.pswrd.value == "Admin") {
-        window.open('https://camzandofel.github.io/Camz-and-Ofel-Admin.github.io/components/dashboard.html')
+        window.open('https://camzandofel.github.io/Camz-and-Ofel-Admin.github.io/components/dashboard.html', "_self")
     } else {
         alert("Error Password or username")
     }
