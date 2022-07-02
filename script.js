e8269b55a021665ff6f74cf190f6637be564fabe
CamzAndOfel <108213420+CamzAndOfel@users.noreply.github.com>
2022-07-02 19:19:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function check(form) {
     if (form.userid.value == "Admin" && form.pswrd.value == "Admin") {
-        window.open('/Camz-and-Ofel-Admin.github.io/components/dashboard.html')
+        window.open('/Camz-and-Ofel-Admin.github.io/components/dashboard.html', ''_self'')
     } else {
         alert("Error Password or username")
     }
