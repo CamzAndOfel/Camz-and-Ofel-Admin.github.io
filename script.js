fd6025f8640d1f54ee4aa871b10909a6dc65b2ab
CamzAndOfel <108213420+CamzAndOfel@users.noreply.github.com>
2022-07-24 23:03:01 +0800

    styling

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function check(form) {
+    // yung admin lang pwedeng palitan
     if (form.userid.value == "Admin" && form.pswrd.value == "Admin") {
-        window.open('https://camzandofel.github.io/Camz-and-Ofel-Admin.github.io/components/dashboard.html', '_self')
+        window.open('http://127.0.0.1:5501/components/dashboard.html', '_self') //https://camzandofel.github.io/Camz-and-Ofel-Admin.github.io/components/dashboard.html
     } else {
         alert("Error Password or username")
     }
