a555e5531770f9bac04859bae41a21d866fd20f7
CamzAndOfel <108213420+CamzAndOfel@users.noreply.github.com>
2022-07-26 15:16:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function check(form) {
     // yung admin lang pwedeng palitan
-    if (form.userid.value == "Admin" && form.pswrd.value == "Admin") {
+    if (form.userid.value == "Mariel" && form.pswrd.value == "Mariel") {
         window.open('https://camzandofel.github.io/Camz-and-Ofel-Admin.github.io/components/dashboard.html', '_self') //
     } else {
         alert("Error Password or username")
